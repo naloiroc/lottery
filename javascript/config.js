3d799cc52b682647c49c425713aa7a324e2ff40d
KamoLee <xu3u835k6@gmail.com>
2026-03-13 13:01:36 +0800

    fix: add config

---
+++ javascript/config.js
@@ -0,0 +1,36 @@
+﻿var info = {
+	act_name: "年終感恩餐會",
+	output_filename: "fatboy_result.csv",
+	can_back_if_not_appear: false,
+	restore_key: new Date().toString().split(' ').join('').substr(3, 9)
+}
+
+var msg_conf = {
+	is_testing: true,
+	admin_sn: "123456",
+	user_id: "admin",
+	api_key: "ABCD1234ABCD1234ABCD1234ABCD1234",
+	password: "123455678",
+	post_text: "請攜帶員工職別證供資料核對，於 1月8日 10:00-17:00依排定時間至辦公室16樓資訊處領獎 。未能於該時段領取獎品者，請於1月15日12:00前，與資訊處小胖子(0988888888)聯絡領取，否則視同放棄領獎。",
+	url: "http://localhost:24680/javascript/msg.js"
+}
+
+var firebase_conf = {
+	//email: "grassboy@is.a.fatboy",
+    //apiKey: "AIzaSyAB8ff11i_AY2o5XN5mYu9OUpPE9Q0j5KI",
+    //authDomain: "lottery-858fe.firebaseapp.com",
+    //databaseURL: "https://lottery-858fe.firebaseio.com",
+    //storageBucket: "lottery-858fe.appspot.com",
+    //messagingSenderId: "260626378859",
+	databaseURL: "https://lottery-6c549-default-rtdb.asia-southeast1.firebasedatabase.app/",
+	email: "xu3u835k6@gmail.com",
+	apiKey: "AIzaSyAypc7yZgWnhMTTdmQynugB62d-jYWf_gc",
+	authDomain: "lottery-6c549.firebaseapp.com",
+	projectId: "lottery-6c549",
+	storageBucket: "lottery-6c549.firebasestorage.app",
+	messagingSenderId: "160458478609",
+	appId: "1:160458478609:web:7f248ade2e77bf12be8db0"
+
+}
+
+var justfont_conf = "<link href='//fonts.googleapis.com/css?family=Asap:700' rel='stylesheet' type='text/css'><script src=\"//s3-ap-northeast-1.amazonaws.com/justfont-user-script/jf-37614.js\"></script>    //<link href='//fonts.googleapis.com/css?family=Asap:700' rel='stylesheet' type='text/css'><script src=\"//s3-ap-northeast-1.amazonaws.com/justfont-user-script/jf-1234.js\"></script>";
